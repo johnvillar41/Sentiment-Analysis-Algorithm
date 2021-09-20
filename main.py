566f9ea1577f98872d40f29612e73d560d85f095
johnvillar41 <johndanielvillar123@gmail.com>
2021-09-20 18:16:22 +0800

    Added Flask and initial commits

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-sample = "Hello World"
-print(sample)
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/hello/', methods=['GET', 'POST'])
+def welcome():
+    return "<h1>Hello World!</h1>"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=105)

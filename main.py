b3bd2af0202c533db4c72a8c2e0d36a1eb324ace
johnvillar41 <johndanielvillar123@gmail.com>
2021-09-21 08:51:32 +0800

    Separated Sentiment Logic to its own class

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from VaderModel import VaderModel
+from SentimentLogic import SentimentLogic
 from flask import Flask
-from flask import jsonify
-from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
 app = Flask(__name__)
 
 
@@ -12,37 +11,10 @@ def welcome(value):
 
 @app.route('/Vader/<string:value>', methods=['GET', 'POST'])
 def Vader(value):
-    return sentiment_scores(value)
+    return SentimentLogic.sentiment_scores_vader(value)
 
 
-def sentiment_scores(sentence):
 
-    # Create a SentimentIntensityAnalyzer object.
-    sid_obj = SentimentIntensityAnalyzer()
-
-    # polarity_scores method of SentimentIntensityAnalyzer
-    # object gives a sentiment dictionary.
-    # which contains pos, neg, neu, and compound scores.
-    sentiment_dict = sid_obj.polarity_scores(sentence)
-
-    negativeVal = sentiment_dict['neg']*100
-    neutralVal = sentiment_dict['neu']*100
-    positiveVal = sentiment_dict['pos']*100
-    compoundVal = sentiment_dict['compound']*100
-    compoundScore = ""
-    if sentiment_dict['compound'] >= 0.05:
-        compoundScore = "Positive"
-
-    elif sentiment_dict['compound'] <= - 0.05:
-        compoundScore = "Negative"
-
-    else:
-        compoundScore = "Neutral"
-
-    vaderModel = VaderModel(negativeVal, positiveVal,
-                            neutralVal, compoundScore, compoundVal)
-
-    return vaderModel.toJSON()
 
 
 if __name__ == '__main__':

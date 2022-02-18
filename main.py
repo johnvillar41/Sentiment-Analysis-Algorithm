4680761e9a4fa77203f2973bbe64912eb3ea9eb2
johnvillar41 <johndanielvillar123@gmail.com>
2022-02-18 14:25:03 +0800

    updated route

--- main.py
+++ main.py
@@ -42,4 +42,4 @@ def Check(value = None):
         return str(TextCleaning.checkIfWordExistOnWordNet(value)).lower()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=105)
+    app.run(host='192.168.1.105', port=105)
